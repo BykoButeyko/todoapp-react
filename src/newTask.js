6826daae5e299d39b3aa7954457703a21ce8f9e4
Byko <bykocrypto@gmail.com>
2021-01-03 23:48:07 +0100

    connect Submit button with backend

--- src/newTask.js
+++ src/newTask.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default() => {
-    return (
-        <div>
-          <h2>New task</h2>
-          <form>
-            <div className="form-group">
-              <label htmlFor="description">Description</label>
-              <textarea className="form-control" id="description"></textarea>
-            </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
-          </form>
+export default ({ createTask }) => {
+  const [description, setDescription] = useState(undefined);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    createTask(description);
+  }
+
+  return (
+    <div>
+      <h2>New task</h2>
+      <form onSubmit={e => handleSubmit(e)}>
+        <div className="form-group">
+          <label htmlFor="description">Description</label>
+          <textarea className="form-control" id="description" onChange={e => setDescription(e.target.value)}></textarea>
         </div>
-    );
-}
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
